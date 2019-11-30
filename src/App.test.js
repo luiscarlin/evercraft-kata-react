0487416df421bcb450696a5fbfa0783cbf4e7d5d
Luis Carlin <luiscarlin@gmail.com>
2019-11-29 21:48:38 -0500

    add name and start alignment

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { shallow } from "enzyme";
+import { findNodeModule } from "jest-resolve";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("App", () => {
+  it('has "Enter Name" label', () => {
+    const subject = shallow(<App />);
+    expect(subject.find(".name label").text()).toEqual("Enter Name");
+  });
+  it("allows user to set the name", () => {
+    const subject = shallow(<App />);
+    subject
+      .find(".name input")
+      .simulate("change", { target: { value: "luis" } });
+
+    expect(subject.find("input").prop("value")).toEqual("luis");
+  });
+
+  it('has "Alignment" label', () => {
+    const subject = shallow(<App />);
+    expect(subject.find(".alignment label").text()).toEqual("Alignment");
+  });
 });
